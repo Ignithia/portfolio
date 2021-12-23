ea3eb7103ae4122a23a0c86bf349d5f77d8b337e
Nxfariouz <killermadness@gmail.com>
2021-12-23 19:26:39 +0100

    Carousel, photo en over mij

--- website/js/cv.js
+++ website/js/cv.js
@@ -1,5 +1,5 @@
 var multipleCardCarousel = document.querySelector(
-    "#carouselExampleControls"
+    "#carousel"
   );
   if (window.matchMedia("(min-width: 768px)").matches) {
     var carousel = new bootstrap.Carousel(multipleCardCarousel, {
@@ -8,16 +8,16 @@ var multipleCardCarousel = document.querySelector(
     var carouselWidth = $(".carousel-inner")[0].scrollWidth;
     var cardWidth = $(".carousel-item").width();
     var scrollPosition = 0;
-    $("#carouselExampleControls .carousel-control-next").on("click", function () {
+    $("#carousel .carousel-control-next").on("click", function () {
       if (scrollPosition < carouselWidth - cardWidth * 4) {
         scrollPosition += cardWidth;
-        $("#carouselExampleControls .carousel-inner").animate(
+        $("#carousel .carousel-inner").animate(
           { scrollLeft: scrollPosition },
           600
         );
       }
     });
-    $("#carouselExampleControls .carousel-control-prev").on("click", function () {
+    $("#carousel .carousel-control-prev").on("click", function () {
       if (scrollPosition > 0) {
         scrollPosition -= cardWidth;
         $("#carouselExampleControls .carousel-inner").animate(
